de63db0ea12958b7add26a4cddc03f1d1840d2a3
zarawu-s <sarajovanovic.aj@gmail.com>
2023-07-06 14:45:39 +0200

    Changed: Dataset used Added: Preprocessing of categorical columns Changed: Results from all 4 algorithms get saved to txt file

--- main.py
+++ main.py
@@ -18,24 +18,29 @@ from pyspark.sql.functions import when
 
 spark = SparkSession.builder.appName("Projekat_3").getOrCreate()
 sc = spark.sparkContext
-data = spark.read.csv('voice.csv', header=True, inferSchema=True)
+data = spark.read.csv('Employee.csv', header=True, inferSchema=True)
 
-# Convert "male" to 0 and "female" to 1 in the "label" column
-data = data.withColumn("label", when(data.label == "male", 0).otherwise(1))
+# Vectorizing categorical columns
+cat_cols = ["Education","City","Gender","EverBenched"]
+assemble_stages = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(data) for column in list(set(cat_cols))]
+pipeline = Pipeline(stages=assemble_stages)
+data = pipeline.fit(data).transform(data)
+data = data.drop("Education","City","Gender","EverBenched")
 
-
-data = data.select(data["label"].cast(DoubleType()).alias("label"))
+data = data.select("Education_index",
+                    data["JoiningYear"].cast(DoubleType()).alias("JoiningYear"),
+                    "City_index",
+                    data["PaymentTier"].cast(DoubleType()).alias("PaymentTier"),
+                    data["Age"].cast(DoubleType()).alias("Age"),                 
+                    "Gender_index",
+                    "EverBenched_index",
+                    data["ExperienceInCurrentDomain"].cast(DoubleType()).alias("ExperienceInCurrentDomain"),
+                    data["label"].cast(DoubleType()).alias("label"))
 
 assembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features")
+data = assembler.transform(data).select("features", "label")
 
-# assembler = VectorAssembler(inputCols=data.columns[1:], outputCol="features")
-
-
-# data = assembler.transform(data).select("features", "label")
-
-data = assembler.transform(data)
-
-
+# Checking if the data set is balanced
 minority_count = data.filter(col("label") == 1).count()
 majority_count = data.filter(col("label") == 0).count()
 size_diff = abs(majority_count - minority_count)
@@ -85,47 +90,98 @@ if(size_diff > 0) :
     minority_count = data.filter(col("label") == 1).count()
     majority_count = data.filter(col("label") == 0).count()
     size_diff = abs(majority_count - minority_count)
-    random_min_rows = data.filter(col("label") == 0).orderBy(rand()).limit(size_diff)
-    data = data.unionAll(random_min_rows)
+    if size_diff != 0 :
+        random_min_rows = data.filter(col("label") == 0).orderBy(rand()).limit(size_diff)
+        data = data.unionAll(random_min_rows)
 
-    minority_count = data.filter(col("label") == 1).count()
-    majority_count = data.filter(col("label") == 0).count()
-    size_diff = abs(majority_count - minority_count)
+        minority_count = data.filter(col("label") == 1).count()
+        majority_count = data.filter(col("label") == 0).count()
+        size_diff = abs(majority_count - minority_count)
     print("Size difference after balancing: {}".format(size_diff))
 
-# # splits = data.randomSplit([0.7, 0.3], 5678)
-# # train = splits[0]
-# # test = splits[1]
+# Random split
+splits = data.randomSplit([0.7, 0.3], 3278)
+train = splits[0]
+test = splits[1]
 
-# # nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
+# Naive Bayes with random split 70%
+nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
+modelNb = nb.fit(train)
+predictions = modelNb.transform(test)
 
-# # classifier = DecisionTreeClassifier(featuresCol="features", labelCol="label")
-# # pipeline = Pipeline(stages=[classifier])
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("Naive Bayes accuracy = " + str(accuracy))
 
-# # classifier = RandomForestClassifier(featuresCol="features", labelCol="label")
-# # pipeline = Pipeline(stages=[classifier])
+evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction", metricName="areaUnderROC")
+auc_roc = evaluator.evaluate(predictions)
+print("Area under ROC curve:", auc_roc) 
 
-# # lr = LogisticRegression()
-# # grid = ParamGridBuilder().addGrid(lr.maxIter, [0, 1]).build()
-# # evaluator = BinaryClassificationEvaluator()
-# # cv = CrossValidator(estimator=pipeline, estimatorParamMaps=grid, evaluator=evaluator,
-# #     parallelism=2)
-# # modelRfc = cv.fit(data)
+predictionAndLabels = predictions.select("prediction", "label").rdd.map(tuple)
+metrics = MulticlassMetrics(predictionAndLabels)
+confusionMatrix = metrics.confusionMatrix()
+print("Confusion Matrix:")
+print(confusionMatrix) 
 
+timestamp = datetime.datetime.now().strftime("%d-%m-%y %H-%M")
+test_results = "{}\nNaive Bayes with random split 70%\nTest set accuracy: {}\nArea under ROC curve: {}\nConfusion Matrix:\n{}\n\n".format(
+    timestamp, accuracy, auc_roc, confusionMatrix)
 
-# # modelNb = cv.fit(data)
-# # modelRfc = cv.fit(data)
-# # modelJ48 = cv.fit(data)
+# Cross validation conf.
+lr = LogisticRegression()
+grid = ParamGridBuilder().addGrid(lr.maxIter, [0, 1]).build()
 
-# # modelNb = nb.fit(train)
-# # modelRfc = pipeline.fit(train)
-# # modelJ48 = pipeline.fit(train)
+# J48 with cross validation, 10 folds
+classifier = DecisionTreeClassifier(featuresCol="features", labelCol="label")
+pipeline = Pipeline(stages=[classifier])
+evaluator = BinaryClassificationEvaluator()
+cv = CrossValidator(estimator=pipeline, estimatorParamMaps=grid, evaluator=evaluator, numFolds=10, parallelism=2)
+model = cv.fit(data)
+predictions = model.transform(data)
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("J48 accuracy = " + str(accuracy))
 
-# # predictions = modelNb.transform(test) #Test set accuracy = 0.6553980370774264 // 0.6728922091782283
-# # predictions = modelRfc.transform(data) #Test set accuracy = 0.8353326063249727 // 0.8585912486659552
-# # predictions = modelJ48.transform(test) #Test set accuracy = 0.8173391494002181 // 0.8628601921024547
+evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction", metricName="areaUnderROC")
+auc_roc = evaluator.evaluate(predictions)
+print("Area under ROC curve:", auc_roc) 
 
-#KNN
+predictionAndLabels = predictions.select("prediction", "label").rdd.map(tuple)
+metrics = MulticlassMetrics(predictionAndLabels)
+confusionMatrix = metrics.confusionMatrix()
+print("Confusion Matrix:")
+print(confusionMatrix) 
+
+timestamp = datetime.datetime.now().strftime("%d-%m-%y %H-%M")
+test_results += "{}\nJ48 with cross validation, 10 folds\nTest set accuracy: {}\nArea under ROC curve: {}\nConfusion Matrix:\n{}\n\n".format(
+    timestamp, accuracy, auc_roc, confusionMatrix)
+
+# Random Forest Classifier with cross validation, 10 folds
+classifier = RandomForestClassifier(featuresCol="features", labelCol="label")
+pipeline = Pipeline(stages=[classifier])
+evaluator = BinaryClassificationEvaluator()
+cv = CrossValidator(estimator=pipeline, estimatorParamMaps=grid, evaluator=evaluator, numFolds=10, parallelism=2)
+model = cv.fit(data)
+predictions = model.transform(data)
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("RFC accuracy = " + str(accuracy))
+
+evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction", metricName="areaUnderROC")
+auc_roc = evaluator.evaluate(predictions)
+print("Area under ROC curve:", auc_roc) 
+
+predictionAndLabels = predictions.select("prediction", "label").rdd.map(tuple)
+metrics = MulticlassMetrics(predictionAndLabels)
+confusionMatrix = metrics.confusionMatrix()
+print("Confusion Matrix:")
+print(confusionMatrix) 
+
+timestamp = datetime.datetime.now().strftime("%d-%m-%y %H-%M")
+test_results += "{}\nRandom Forest Classifier with cross validation, 10 folds\nTest set accuracy: {}\nArea under ROC curve: {}\nConfusion Matrix:\n{}\n\n".format(
+    timestamp, accuracy, auc_roc, confusionMatrix)
+
+# KNN 
 # rdd = data.rdd.map(lambda row: (Vectors.dense(row.features.toArray()), row.label))
 # X = np.array(rdd.map(lambda x: x[0]).collect())
 # y = np.array(rdd.map(lambda x: x[1]).collect())
@@ -133,10 +189,6 @@ if(size_diff > 0) :
 data_array = data.rdd.map(lambda row: (row.features.toArray(), row.label)).collect()
 X = np.array([x[0] for x in data_array])
 y = np.array([x[1] for x in data_array])
-
-
-
-
 
 # Print the shape of X and y to check if they have the expected dimensions
 print(X.shape)
@@ -154,29 +206,25 @@ prediction_df = prediction_rdd.toDF(["prediction", "label"])
 
 evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
 accuracy = evaluator.evaluate(prediction_df)
-print("Test set accuracy = " + str(accuracy)) # Test set accuracy = 0.88921875
+print("Test set accuracy = " + str(accuracy)) 
 
 evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction", metricName="areaUnderROC")
 auc_roc = evaluator.evaluate(prediction_df)
-print("Area under ROC curve:", auc_roc) # Area under ROC curve: 0.88921875
+print("Area under ROC curve:", auc_roc) 
 
 predictionAndLabels = prediction_df.select("prediction", "label").rdd.map(tuple)
 metrics = MulticlassMetrics(predictionAndLabels)
 confusionMatrix = metrics.confusionMatrix()
 print("Confusion Matrix:")
-print(confusionMatrix) #DenseMatrix([[3012.,  188.],
-                                    #[ 521., 2679.]])
-
+print(confusionMatrix) 
 
 timestamp = datetime.datetime.now().strftime("%d-%m-%y %H-%M")
-test_results = "Test set accuracy: {}\nArea under ROC curve: {}\nConfusion Matrix:\n{}".format(
-    accuracy, auc_roc, confusionMatrix)
+test_results += "{}\nKNN\nTest set accuracy: {}\nArea under ROC curve: {}\nConfusion Matrix:\n{}".format(
+    timestamp, accuracy, auc_roc, confusionMatrix)
 print(test_results)
 
 file_name = timestamp + " test_results.txt"
 with open(file_name, 'w') as file:
     file.write(test_results)
-
-
 
 spark.stop()
